0e0c2da630d00726754c704a9093f3efb2cc81c4
unknown <sapek.arkadiusz@gmail.com>
2018-08-30 02:57:14 +0200

    5"

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,11 +1,7 @@
 class Constants {
   public API_BASE;
   constructor() {
-    if (process.env.NODE_ENV === "production") {
-      this.API_BASE = "https://event-list-server.herokuapp.com/";
-    } else {
-      this.API_BASE = "http://localhost:4000/api";
-    }
+    this.API_BASE = "https://event-list-server.herokuapp.com/";
   }
 }
 
